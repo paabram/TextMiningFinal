b29a2db7d11f927392cd816834b53d5f56afd603
paabram <patrick.w.aabram@gmail.com>
2025-11-14 12:09:11 -0500

    initial commit / preliminary analysis

---
+++ dataEngineering.R
@@ -0,0 +1,59 @@
+#install.packages(c("tidyverse", "tidytext")) 
+library(tidyverse)
+library(tidytext)
+
+Fake <- read_csv("data/Fake.csv")
+True <- read_csv("data/True.csv")
+
+#tokenize the text column into words
+fake_words <- Fake %>%
+  select(text) %>%           # use the article body
+  unnest_tokens(word, text)  # lowercases, no punctuation, splits into words
+
+true_words <- True %>%
+  select(text) %>%
+  unnest_tokens(word, text)
+
+# Remove common stopwords
+data("stop_words")
+
+fake_words <- fake_words %>%
+  anti_join(stop_words, by = "word")
+
+true_words <- true_words %>%
+  anti_join(stop_words, by = "word")
+
+#frequency tables
+fake_freq <- fake_words %>%
+  count(word, sort = TRUE) %>%
+  rename(fake_count = n)
+
+true_freq <- true_words %>%
+  count(word, sort = TRUE) %>%
+  rename(true_count = n)
+
+head(fake_freq, 20)   # top 20 in Fake
+head(true_freq, 20)   # top 20 in True
+
+#difference column
+comparison <- full_join(fake_freq, true_freq, by = "word") %>%
+  replace_na(list(fake_count = 0, true_count = 0)) %>%
+  mutate(
+    total = fake_count + true_count,
+    diff  = fake_count - true_count   # >0 = more common in Fake; <0 = more common in True
+  ) %>%
+  arrange(desc(total))
+
+head(comparison, 20)
+
+# Words most skewed toward Fake (with at least some overall frequency)
+comparison %>%
+  filter(total > 5000) %>%
+  arrange(desc(diff)) %>%
+  head(20)
+
+# Words most skewed toward True
+comparison %>%
+  filter(total > 5000) %>%
+  arrange(diff) %>%
+  head(20)
