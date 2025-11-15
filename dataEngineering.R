c784ce058bbce225c8bb528c4c301c890d408ca0
paabram <patrick.w.aabram@gmail.com>
2025-11-15 13:31:41 -0500

    adds proposal document

--- dataEngineering.R
+++ dataEngineering.R
@@ -1,9 +1,22 @@
 #install.packages(c("tidyverse", "tidytext")) 
 library(tidyverse)
 library(tidytext)
+library(stringr)
 
 Fake <- read_csv("data/Fake.csv")
 True <- read_csv("data/True.csv")
+
+# exploration
+word_counts_F <- str_count(Fake$text, "\\S+")
+max(word_counts_F, na.rm = T)
+min(word_counts_F, na.rm = T) # will need to remove articles with little/missing body
+mean(word_counts_F, na.rm = T)
+
+word_counts_T <- str_count(True$text, "\\S+")
+max(word_counts_T, na.rm = T)
+min(word_counts_T, na.rm = T)
+mean(word_counts_T, na.rm = T)
+
 
 #tokenize the text column into words
 fake_words <- Fake %>%
